d9653e0acbabbe26e4aa60b8f85976c98da7d0c2
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-13 19:59:34 -0400

    add task4.js

--- js/task-4.js
+++ js/task-4.js
@@ -5,8 +5,8 @@ registerForm.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const form = event.target;
-  const login = form.elements.email.value;
-  const password = form.elements.password.value;
+  const login = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
   if (login === '' || password === '') {
     window.alert('Please fill in all the fields!');
