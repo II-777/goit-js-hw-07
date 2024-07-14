1e066dfbb6df1ded2d7f871b1ce76ae30a91a8d7
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-13 21:20:34 -0400

    add task6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
+}
+
+const btnDestroy = document.getElementById('data-destroy');
+const btnCreate = document.getElementById('data-create');
+const inputNumber = document.getElementById('input');
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = parseInt(inputNumber.value);
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById('boxes');
+  boxesContainer.innerHTML = '';
 }
