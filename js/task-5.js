b726b0e63be4de10d962b53c3abe6dfe845e54af
II-777 <45958058+II-777@users.noreply.github.com>
2024-07-13 20:35:20 -0400

    add task5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+
+const handleClick = (event) => {
+  const color = getRandomHexColor();
+  const body = document.body;
+  const spanElement = document.querySelector('.color');
+  spanElement.textContent = color;
+  body.style.backgroundColor = color;
+};
+
+button.addEventListener('click', handleClick);
